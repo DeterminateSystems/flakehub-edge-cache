999ba438e4ec5d491309e03ee3355066bd61e972
Cole Mickens <cole.mickens@determinate.systems>
2026-07-29 15:48:06 -0500

    modulues/nixos: combine config files

--- modules/default.nix
+++ modules/default.nix
@@ -10,64 +10,6 @@ let
   nginxDurationType = lib.types.strMatching "^[0-9]+[dhms]$";
   nginxSizeType = lib.types.strMatching "^[0-9]+[kmg]$";
 
-  edgeConfiguration = builtins.toFile "fhc-edge.conf" ''
-    server {
-      ${lib.concatMapStringsSep "\n  " (addr: "listen ${addr} default_server;") cfg.listen}
-
-      # Ignored by nginx since we're using default_server for our listening
-      server_name _;
-
-      # Resolve the cache address per request so address changes do not require an nginx reload.
-      resolver ${toString cfg.dnsResolvers} valid=5s ipv4=on ipv6=${
-        if cfg.dnsResolverIPv6 then "on" else "off"
-      };
-      ${lib.optionalString (
-        cfg.upstreamResolveTimeout != null
-      ) "resolver_timeout ${cfg.upstreamResolveTimeout};"}
-
-      location = /nix-cache-info {
-        return 200 "WantMassQuery: 1\nStoreDir: /nix/store\nPriority: 30\n"; # Higher priority than FHC (39) and cache.nixos.org (40)
-      }
-
-      # Pass requests for narinfo directly to FlakeHub Cache, but never cache it
-      location ~ /.*?\.narinfo$ {
-        set $fhc_upstream https://cache.flakehub.com;
-        proxy_pass $fhc_upstream;
-        proxy_ssl_server_name on;
-        proxy_ssl_name cache.flakehub.com;
-        ${lib.optionalString cfg.sslVerify "proxy_ssl_verify on;"}
-        ${lib.optionalString cfg.sslVerify "proxy_ssl_trusted_certificate \"${cfg.sslTrustedCertificate}\";"}
-        ${lib.optionalString cfg.sslVerify "proxy_ssl_verify_depth ${toString cfg.sslVerifyDepth};"}
-        proxy_set_header Host cache.flakehub.com;
-        ${lib.optionalString (
-          cfg.upstreamResolveTimeout != null
-        ) "proxy_connect_timeout ${cfg.upstreamResolveTimeout};"}
-        ${lib.optionalString (
-          cfg.upstreamReadTimeout != null
-        ) "proxy_read_timeout ${cfg.upstreamReadTimeout}; proxy_send_timeout ${cfg.upstreamReadTimeout};"}
-      }
-
-      # Allow caching of any request for a NAR
-      location /nar {
-        proxy_cache fhc;
-        proxy_cache_valid ${cfg.cacheLifetime};
-        ${lib.optionalString cfg.cacheLock "proxy_cache_lock on; proxy_cache_lock_timeout ${cfg.cacheLockTimeout}; proxy_cache_lock_age ${cfg.cacheLockAge};"}
-
-        set $fhc_upstream https://cache.flakehub.com;
-        proxy_pass $fhc_upstream;
-        proxy_ssl_server_name on;
-        proxy_ssl_name cache.flakehub.com;
-        ${lib.optionalString cfg.sslVerify "proxy_ssl_verify on;"}
-        ${lib.optionalString cfg.sslVerify "proxy_ssl_trusted_certificate \"${cfg.sslTrustedCertificate}\";"}
-        ${lib.optionalString cfg.sslVerify "proxy_ssl_verify_depth ${toString cfg.sslVerifyDepth};"}
-        proxy_set_header Host cache.flakehub.com;
-        ${lib.optionalString (
-          cfg.upstreamResolveTimeout != null
-        ) "proxy_connect_timeout ${cfg.upstreamResolveTimeout};"}
-      }
-    }
-  '';
-
   nginxConfiguration = builtins.toFile "fhc-edge-nginx.conf" ''
     error_log stderr;
 
@@ -110,7 +52,61 @@ let
         ${lib.optionalString (cfg.minCacheFree != null) "min_free=${cfg.minCacheFree}"}
         ;
 
-      include ${edgeConfiguration};
+      server {
+        ${lib.concatMapStringsSep "\n  " (addr: "listen ${addr} default_server;") cfg.listen}
+
+        # Ignored by nginx since we're using default_server for our listening
+        server_name _;
+
+        # Resolve the cache address per request so address changes do not require an nginx reload.
+        resolver ${toString cfg.dnsResolvers} valid=5s ipv4=on ipv6=${
+          if cfg.dnsResolverIPv6 then "on" else "off"
+        };
+        ${lib.optionalString (
+          cfg.upstreamResolveTimeout != null
+        ) "resolver_timeout ${cfg.upstreamResolveTimeout};"}
+
+        location = /nix-cache-info {
+          return 200 "WantMassQuery: 1\nStoreDir: /nix/store\nPriority: 30\n"; # Higher priority than FHC (39) and cache.nixos.org (40)
+        }
+
+        # Pass requests for narinfo directly to FlakeHub Cache, but never cache it
+        location ~ /.*?\.narinfo$ {
+          set $fhc_upstream https://cache.flakehub.com;
+          proxy_pass $fhc_upstream;
+          proxy_ssl_server_name on;
+          proxy_ssl_name cache.flakehub.com;
+          ${lib.optionalString cfg.sslVerify "proxy_ssl_verify on;"}
+          ${lib.optionalString cfg.sslVerify "proxy_ssl_trusted_certificate \"${cfg.sslTrustedCertificate}\";"}
+          ${lib.optionalString cfg.sslVerify "proxy_ssl_verify_depth ${toString cfg.sslVerifyDepth};"}
+          proxy_set_header Host cache.flakehub.com;
+          ${lib.optionalString (
+            cfg.upstreamResolveTimeout != null
+          ) "proxy_connect_timeout ${cfg.upstreamResolveTimeout};"}
+          ${lib.optionalString (
+            cfg.upstreamReadTimeout != null
+          ) "proxy_read_timeout ${cfg.upstreamReadTimeout}; proxy_send_timeout ${cfg.upstreamReadTimeout};"}
+        }
+
+        # Allow caching of any request for a NAR
+        location /nar {
+          proxy_cache fhc;
+          proxy_cache_valid ${cfg.cacheLifetime};
+          ${lib.optionalString cfg.cacheLock "proxy_cache_lock on; proxy_cache_lock_timeout ${cfg.cacheLockTimeout}; proxy_cache_lock_age ${cfg.cacheLockAge};"}
+
+          set $fhc_upstream https://cache.flakehub.com;
+          proxy_pass $fhc_upstream;
+          proxy_ssl_server_name on;
+          proxy_ssl_name cache.flakehub.com;
+          ${lib.optionalString cfg.sslVerify "proxy_ssl_verify on;"}
+          ${lib.optionalString cfg.sslVerify "proxy_ssl_trusted_certificate \"${cfg.sslTrustedCertificate}\";"}
+          ${lib.optionalString cfg.sslVerify "proxy_ssl_verify_depth ${toString cfg.sslVerifyDepth};"}
+          proxy_set_header Host cache.flakehub.com;
+          ${lib.optionalString (
+            cfg.upstreamResolveTimeout != null
+          ) "proxy_connect_timeout ${cfg.upstreamResolveTimeout};"}
+        }
+      }
     }
   '';
 in
